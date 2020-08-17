19ee0898b846651ed97e689148f6c6b5fa22d4e1
taixcms <admin@taix.ru>
2020-08-17 10:58:56 +0300

    Hello, test package

--- src/digitalsamba.php
+++ src/digitalsamba.php
@@ -8,39 +8,35 @@ namespace phputilities;
  * Time: 15:21
  */
 class digitalsamba {
-	const API_EndPoint = 'http://cbrates.rbc.ru/tsv/';
-	const Authorization = 'YWRtaW46c2FtYmEyMDAy';
+//	const API_EndPoint = 'http://domain.com/';
+//	const Authorization = 'YWRtaW46c2FtYmEyMDAy';
 
 	public function __construct() {
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "{{API_EndPoint}}/api/2/{{user_name}}/authverify",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_POSTFIELDS =>"input_type=json&rest_data={\n  \"id\":\"1\"\n}",
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: Basic YWRtaW46c2FtYmEyMDAy"
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        echo $response;
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => "{{API_EndPoint}}/api/2/{{user_name}}/authverify",
+//            CURLOPT_RETURNTRANSFER => true,
+//            CURLOPT_ENCODING => "",
+//            CURLOPT_MAXREDIRS => 10,
+//            CURLOPT_TIMEOUT => 0,
+//            CURLOPT_FOLLOWLOCATION => true,
+//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//            CURLOPT_CUSTOMREQUEST => "GET",
+//            CURLOPT_POSTFIELDS =>"input_type=json&rest_data={\n  \"id\":\"1\"\n}",
+//            CURLOPT_HTTPHEADER => array(
+//                "Authorization: Basic {{Authorization}}"
+//            ),
+//        ));
+//
+//        $response = curl_exec($curl);
+//
+//        curl_close($curl);
+//        echo $response;
 	}
 
 	public function auth() {
-
-	}
-
-	private function parseUA() {
-
+         echo 'Hello 123';
 	}
 
 }
